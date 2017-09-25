f78befdcd884ec1b3bc3add97bcd5a739bf56366
marcqleonard <s3579725@student.rmit.edu.au>
2017-09-25 23:11:17 +0200

    Added UserViewDetails test case

--- src/Tests/Python/models/api_facade.py
+++ src/Tests/Python/models/api_facade.py
@@ -5,12 +5,15 @@ from .user_investor import User
 from .endpoints.registration import Registration
 from .endpoints.authentication import Authentication
 from .endpoints.deletion import Deletion
+from .endpoints.view_details import ViewDetails
+
 
 
 API_URL = "https://investor-api.herokuapp.com/api/1.0"
 REGISTER_URL = API_URL + "/users"
 AUTHENTICATION_URL = API_URL + "/token"
 DELETION_URL = API_URL + "/users"
+VIEW_DETAILS_URL = API_URL + "/users"
 
 
 class ApiFacade:
@@ -31,4 +34,8 @@ class ApiFacade:
 	def delete_user(self):
 		deletion = Deletion(DELETION_URL, self.user, self.token)
 		return deletion.get_outcome()
+
+	def view_details(self):
+		view_details = ViewDetails(VIEW_DETAILS_URL, self.user, self.token)
+		return view_details.get_outcome()
 

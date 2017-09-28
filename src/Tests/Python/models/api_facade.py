d5e8c71e7f3b2d992ec0249593203f68c7ef436a
marcqleonard <s3579725@student.rmit.edu.au>
2017-09-28 05:39:34 +0200

    Removed state in API facade to make tests more independent

--- src/Tests/Python/models/api_facade.py
+++ src/Tests/Python/models/api_facade.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from .user_investor import User
 from .endpoints.registration import Registration
 from .endpoints.authentication import Authentication
 from .endpoints.deletion import Deletion
-from .endpoints.view_details import ViewDetails
-
-
 
 API_URL = "https://investor-api.herokuapp.com/api/1.0"
 REGISTER_URL = API_URL + "/users"
@@ -15,27 +11,23 @@ AUTHENTICATION_URL = API_URL + "/token"
 DELETION_URL = API_URL + "/users"
 VIEW_DETAILS_URL = API_URL + "/users"
 
-
 class ApiFacade:
-	def __init__(self, user, token = None):
-		self.user = user
-		self.token = token
+	def __init__(self):
+		pass
 
-	def register_user(self):
-		registration = Registration(REGISTER_URL, self.user)
+	def register_user(self, displayName, email, password):
+		registration = Registration(REGISTER_URL, displayName, email, password)
 		return registration.get_outcome()
 
-	def authenticate_user(self):
-		authentication = Authentication(AUTHENTICATION_URL, self.user)
-		if authentication.get_outcome().is_success:
-			self.token = authentication.get_token()
+	def authenticate_user(self, email, password):
+		authentication = Authentication(AUTHENTICATION_URL, email, password)
 		return authentication.get_outcome()
 
-	def delete_user(self):
-		deletion = Deletion(DELETION_URL, self.user, self.token)
+	def delete_user(self, token):
+		deletion = Deletion(DELETION_URL, token)
 		return deletion.get_outcome()
 
-	def view_details(self):
-		view_details = ViewDetails(VIEW_DETAILS_URL, self.user, self.token)
-		return view_details.get_outcome()
+	# def view_details(self, expected_status, user):
+	# 	view_details = ViewDetails(VIEW_DETAILS_URL, user, self.token)
+	# 	return view_details.get_outcome()
 

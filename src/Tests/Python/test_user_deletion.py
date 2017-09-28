6b6daee621e5eb261430ec2f0c911159ab2a188e
marcqleonard <s3579725@student.rmit.edu.au>
2017-09-28 15:33:16 +0200

    User account deletion fixed

--- src/Tests/Python/test_user_deletion.py
+++ src/Tests/Python/test_user_deletion.py
@@ -22,16 +22,18 @@ class UserDeletionTestCase(unittest.TestCase):
 	def test_deletion_success(self):
 		"""An authenticated user can delete their user account"""
 		api = ApiFacade()
-		response_code = api.register_user("John Doe", "johndoe@test.com", "12345678")
-		response_code, token = api.authenticate_user("johndoe@test.com", "12345678")
+		user_params = ("John Doe", "johndoe@test.com", "12345678")
+		response_code = api.register_user(*user_params)
+		response_code, token = api.authenticate_user(*user_params[1:3])
 		response_code = api.delete_user(token)
-		self.assertEqual(response_code, 204, msg = "fail")
+		self.assertEqual(response_code, 204, msg = "For data: [{0}]; Got: [HTTP {1}]; Expected: [HTTP {2}]"
+			.format(';'.join(user_params), response_code, 204))
 
 	def test_deletion_userIsNotAuthenticated(self):
 		"""An unauthenticated user cannot delete their user account"""
 		api = ApiFacade()
 		response_code = api.delete_user(token = None)
-		self.assertEqual(response_code, 401, msg = "fail")
+		self.assertEqual(response_code, 401, msg = "Got: [HTTP {0}]; Expected: [HTTP {1}]".format(response_code, 401))
 
 if __name__ == "__main__":
 	unittest.main()

f22b7596d4bc5dce333596c03f34a5d13c185a1a
marcqleonard <s3579725@student.rmit.edu.au>
2017-09-29 19:59:31 +0200

    Response validator updated to support collections and single items in response

--- src/Tests/Python/models/response_validator.py
+++ src/Tests/Python/models/response_validator.py
@@ -136,44 +136,83 @@ class ResponseValidator:
 		except ValueError:
 			self.server_response_body = None
 
-		self.errors = []
+		self.load_errors = []
+		self.body_errors = []
 
 	def validate_response_code(self, server_response_code, acceptable_response_code):
 		if server_response_code == acceptable_response_code:
 			passed = True
 		else:
 			passed = False
-			self.errors.append("Expected HTTP {0}; got HTTP {1}".format(acceptable_response_code, server_response_code))
-		return passed
+			self.load_errors.append("Expected HTTP {0}; got HTTP {1}".format(acceptable_response_code, server_response_code))
+		return (passed, server_response_code)
 
 	def validate_response_body_driver(self, server_json, acceptable_model):
-		if server_json is None: return False
-		body_errors = 0
-		if acceptable_model["is_collection"]:
+		if server_json is None and acceptable_model is not None: 
+			self.body_errors.append("No body in response")
+			return False
+		if server_json is not None and acceptable_model is None:
+			self.body_errors.append("Expected no response body; got: {0}".format(server_json))
+			return False
+		if server_json is None and acceptable_model is None:
+			return True
+
+		# body_errors = 0
+		# if acceptable_model["is_collection"]:
+		# 	if "accept_empty" in acceptable_model:
+		# 		if not acceptable_model["accept_empty"] and len(server_json) == 0:
+		# 			body_errors += 1
+		# 			self.errors.append("Expected item in collection but got none")
+		# 			return False
+		# 	for item in server_json:
+		# 		for key in acceptable_model["model"]:
+		# 			if key not in item:
+		# 				body_errors += 1
+		# 				self.errors.append("Missing key [{0}]".format(key))
+		# 			else:
+		# 				if not acceptable_model["model"][key]["key_only"]:
+		# 					self.validate_response_body_driver(item[key], acceptable_model["model"][key])
+		# else:
+		# 	if isinstance(acceptable_model["value"], list):
+		# 		if server_json not in acceptable_model["value"]:
+		# 			body_errors += 1
+		# 			self.errors.append("Expected value: [{0}]; Received value: [{1}] instead".format(acceptable_model["value"], server_json))
+		# 	elif server_json != acceptable_model["value"]:
+		# 		body_errors += 1
+		# 		self.errors.append("Expected value: [{0}]; Received value: [{1}] instead".format(acceptable_model["value"], server_json))
+
+		# if wrapper is collection
+		if "is_collection" in acceptable_model and acceptable_model["is_collection"]:
+
+			# if collection can be empty
 			if "accept_empty" in acceptable_model:
 				if not acceptable_model["accept_empty"] and len(server_json) == 0:
-					body_errors += 1
-					self.errors.append("Expected item in collection but got none")
+					self.body_errors.append("Expected item in collection but got none")
 					return False
-			for item in server_json:
-				for key in acceptable_model["model"]:
-					if key not in item:
-						body_errors += 1
-						self.errors.append("Missing key [{0}]".format(key))
-					else:
-						if not acceptable_model["model"][key]["key_only"]:
-							self.validate_response_body_driver(item[key], acceptable_model["model"][key])
-		else:
-			# several values accepted
-			if isinstance(acceptable_model["value"], list):
-				if server_json not in acceptable_model["value"]:
-					body_errors += 1
-					self.errors.append("Expected value: [{0}]; Received value: [{1}] instead".format(acceptable_model["value"], server_json))
-			elif server_json != acceptable_model["value"]:
-				body_errors += 1
-				self.errors.append("Expected value: [{0}]; Received value: [{1}] instead".format(acceptable_model["value"], server_json))
 
-		if body_errors != 0:
+			# for each item in the collection
+			for item in server_json:
+				# go through each mandatory key defined in model
+				for key in acceptable_model["model"]:
+					# if mandatory key not in json item
+					if key not in item:
+						self.body_errors.append("Missing key [{0}]".format(key))
+					# mandatory key is in json item
+					else:
+						# compare values if not "key_only" check
+						if not acceptable_model["model"][key]["key_only"]:
+							self.validate_response_body_driver({key: item[key]}, {key: acceptable_model["model"][key]})
+		else:
+			for key in acceptable_model:
+				if key not in server_json:
+					self.body_errors.append("Missing key: [{0}]".format(key))
+				elif isinstance(acceptable_model[key]["value"], list):
+					if server_json[key] not in acceptable_model[key]["value"]:
+						self.body_errors.append("Expected value: [{0}]; Received value: [{1}] instead".format(acceptable_model[key]["value"], server_json[key]))
+				elif server_json[key] != acceptable_model[key]["value"]:
+					self.body_errors.append("Expected value: [{0}]; Received value: [{1}] instead".format(acceptable_model[key]["value"], server_json[key]))
+
+		if len(self.body_errors) != 0:
 			return False
 
 		return True
@@ -185,78 +224,7 @@ class ResponseValidator:
 		return self.validate_response_body_driver(self.server_response_body, self.acceptable_response_model)
 
 	def get_errors(self):
-		return self.errors
-
-
-
-
-
-# class ResponseValidator:
-# 	def __init__(self, server_response, acceptable_response_code, acceptable_keys_values, 
-# 		check_keys_only = False):
-# 		self.acceptable_response_code = acceptable_response_code
-# 		self.acceptable_keys_values = acceptable_keys_values
-# 		self.check_keys_only = check_keys_only
-# 		self.server_response_code = server_response.status_code
-# 		try:
-# 			self.server_response_body = server_response.json()
-# 		except ValueError:
-# 			self.server_response_body = None
-
-# 		self.errors = []
-
-# 	def validate_response_code(self, server_response_code, acceptable_response_code):
-# 		if server_response_code == acceptable_response_code:
-# 			passed = True
-# 		else:
-# 			passed = False
-# 			self.errors.append("Expected HTTP {0}; got HTTP {1}".format(acceptable_response_code, server_response_code))
-
-# 		return passed
-
-# 	def validate_response_body(self, server_response_body, acceptable_keys_values, is_collection = False, check_keys_only = False):
-		
-# 		# if is_collection:
-# 		# 	for item in server_response_body:
-# 		# 		if isinstance(item, list): 
-# 		# 			is_collection = True
-# 		# 		else:
-# 		# 			is_collection = False
-# 		# 		validate_response_body(server_response_body, acceptable_keys_values, is_collection, check_keys_only)
-
-# 		error_count = 0
-# 		if acceptable_keys_values is None:
-# 			return True
-# 		elif server_response_body is None:
-# 			self.errors.append("Response body is empty")
-# 			error_count += 1
-# 		else:
-# 			for key in acceptable_keys_values:
-# 				if key not in server_response_body:
-# 					self.errors.append("Missing key {0}".format(key))
-# 					error_count += 1
-# 				elif not check_keys_only:
-# 					if isinstance(acceptable_keys_values[key], list):
-# 						if server_response_body[key] not in acceptable_keys_values[key]:
-# 							self.errors.append("Incorrect value for key {0}; expected {1}; got {2}".format(key, " | ".join(acceptable_keys_values[key]), server_response_body[key]))
-# 							error_count += 1
-# 					elif acceptable_keys_values[key] != server_response_body[key]:
-# 						self.errors.append("Incorrect value for key {0}; expected {1}; got {2}".format(key, acceptable_keys_values[key], server_response_body[key]))
-# 						error_count += 1
-
-# 		if error_count != 0:
-# 			return False
-# 		else:
-# 			return True
-
-# 	def response_code_success(self):
-# 		return self.validate_response_code(self.server_response_code, self.acceptable_response_code)
-
-# 	def response_body_success(self):
-# 		return self.validate_response_body(self.server_response_body, self.acceptable_keys_values, self.check_keys_only)
-
-# 	def get_errors(self):
-# 		return self.errors
+		return self.load_errors + self.body_errors
 
 if __name__ == "__main__":
 	validator = ResponseValidator(SERVER_RESPONSE, 200, RESPONSE_MODEL)
